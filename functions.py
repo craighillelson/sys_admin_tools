de9ea0bb1257e3f335090bf76e3d8480304e423e
Craig Hillelson <craighillelson@gmail.com>
2020-09-20 14:56:29 -0400

    add functions.py
    first commit

---
+++ functions.py
@@ -0,0 +1,14 @@
+"""Functions."""
+
+import os
+import pyinputplus as pyip
+
+
+def get_hostname():
+    """Prompt user for a hostname."""
+    return input("\nenter a hostname\n> ")
+
+
+def print_return():
+    """Print a return."""
+    print("\n")
