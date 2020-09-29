069575ce132f14c1b98d39c929252e6b1feb285a
Craig Hillelson <craighillelson@gmail.com>
2020-09-29 00:25:40 -0400

    update system_details.py
    added uptime

--- system_details.py
+++ system_details.py
@@ -9,6 +9,7 @@ print(f"release: {specs.release}")
 print(f"network name: {specs.node}")
 processor = platform.processor()
 print(f"processor: {processor}")
+os.system(f"echo 'uptime:' $(uptime)")
 os.system(f"echo 'public ip address:' "
           f"$(dig +short myip.opendns.com @resolver1.opendns.com)")
 os.system(f"echo 'private ip address: '$(ifconfig | grep 'inet ' | "
