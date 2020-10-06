6689ca6e116c5aed4412f1058f4652edb8927d02
Craig Hillelson <craighillelson@gmail.com>
2020-10-06 07:53:31 -0400

    update network.py
    added functionality to get external, internal ip addresses, the network card's MAC address, and the default gateway.

--- network.py
+++ network.py
@@ -1,6 +1,11 @@
-"""Send a request to a Web site to check check connectivity."""
+"""
+Send a request to a Web site to check check connectivity and gather network 
+details.
+"""
 
 import requests
+import socket
+import netifaces as ni
 
 
 def check_connectivity():
@@ -12,3 +17,25 @@ if check_connectivity() == True:
     print("good to go")
 else:
     print("something is off")
+
+print("\nNetworking Details")
+
+hostname = socket.gethostname()
+print(f"Hostname: {hostname}")
+
+external_ip = requests.get("https://api.ipify.org").text
+print(f"External IP: {external_ip}")
+
+ni.ifaddresses('en0')
+ip = ni.ifaddresses('en0')[ni.AF_INET][0]['addr']
+print(f"Internal IP: {ip}")
+
+import netifaces as ni
+addrs = ni.ifaddresses('en0')
+addrs[ni.AF_INET]
+mac = addrs[ni.AF_LINK]
+for mac_addrs in mac[0].values():
+    print(f"MAC address: {mac_addrs}")
+
+gws = ni.gateways()[ni.AF_INET][0]
+print(f"Gateway: {gws[0]}\n")
