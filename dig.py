11d1c344446bedf853a4a70894b13eba9bbb2cce
Craig Hillelson <craighillelson@gmail.com>
2020-10-10 15:29:44 -0400

    update dig.py
    edited import statements

--- dig.py
+++ dig.py
@@ -1,7 +1,7 @@
 """Prompt user for a hostname and run dig for that hostname."""
 
-from functions import (get_hostname,
-                       os)
+import os
+from functions import get_hostname
 
 hostname = get_hostname()
 response = os.system(f"dig {hostname}")
