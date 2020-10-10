33f62fb5cfc5d19710904caaf3f5a2624776b93e
Craig Hillelson <craighillelson@gmail.com>
2020-10-10 08:40:37 -0400

    update dig.py
    removed unnecessary import

--- dig.py
+++ dig.py
@@ -1,8 +1,7 @@
 """Prompt user for a hostname and run dig for that hostname."""
 
 from functions import (get_hostname,
-                       os,
-                       print_return)
+                       os)
 
 hostname = get_hostname()
 response = os.system(f"dig {hostname}")
