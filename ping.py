4d35a38b1b2cfffc34ea0697856e0a57a831a42d
Craig Hillelson <craighillelson@gmail.com>
2021-07-08 20:48:23 -0400

    update ping
    renamed functions to make them more descriptive

--- ping.py
+++ ping.py
@@ -1,16 +1,27 @@
 """Prompt user for a hostname to ping and a number of pings to send."""
 
 import os
+import sys
 import pyinputplus as pyip
-from functions import (get_hostname,
-                       print_return)
+from functions import get_hostname
+from tee import StdoutTee
+
+
+def get_results():
+    print("\n")
+    return os.system(f"\nping -c {num_pings} {hostname}")
+
+
+def output_results():
+    with StdoutTee("ping.txt", "a", 1):
+        if response == 0:
+            sys.stdout.write(f"\n{hostname} is up")
+        else:
+            sys.stdout.write(f"\n{hostname} is down")
+        print("\n")
+
 
 hostname = get_hostname()
 num_pings = pyip.inputInt("enter a number pings\n> ")
-print_return()
-
-response = os.system(f"ping -c {num_pings} {hostname}")
-if response == 0:
-    print(f"\n{hostname} is up\n")
-else:
-    print(f"\n{hostname} is down\n")
+response = get_results()
+output_results()
